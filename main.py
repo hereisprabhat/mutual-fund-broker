2ef92d082128e319a6905d10e63d90c8686adf56
Prabhat Sharma <prabhatsharma@Prabhats-MacBook-Air.local>
2024-09-03 01:47:26 +0530

    mutual fund broker FE dashboard added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+from typing import List
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -22,9 +23,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Configuration constants
+RAPID_API_KEY = os.getenv("RAPID_API_KEY")
+RAPID_API_URL = os.getenv("RAPID_API_URL")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 
-headers = {"X-RapidAPI-Key": os.getenv("RAPID_API_KEY"), "X-RapidAPI-Host": os.getenv("RAPID_API_URL")}
-base_url = "https://" + os.getenv("RAPID_API_URL")
+headers = {"X-RapidAPI-Key": RAPID_API_KEY, "X-RapidAPI-Host": RAPID_API_URL}
+base_url = f"https://{RAPID_API_URL}"
+
+def fetch_data(url: str, params: dict) -> List[dict]:
+    """
+    Fetch data from the given URL with specified query parameters.
+    """
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    raise HTTPException(status_code=response.status_code, detail="Failed to fetch data")
 
 
 @app.post("/login")
@@ -32,10 +48,9 @@ def login(user_data: UserLogin):
     """
     endpoint for dummy login and returning the dummpy access token
     """
-    if user_data.username == os.getenv("USERNAME") and user_data.password == os.getenv("PASSWORD"):
-        return {"access_token": os.getenv("ACCESS_TOKEN")}
-    else:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+    if user_data.username == USERNAME and user_data.password == PASSWORD:
+        return {"access_token": ACCESS_TOKEN}
+    raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
 @app.get("/fund-families", dependencies=[Depends(authenticate_access_token)])
@@ -43,25 +58,13 @@ def get_fund_families():
     """
     Endpoint to get all the fund details
     """
-    url = base_url + "/latest"
+    url = f"{base_url}/latest"
+    params = {"Scheme_Type": "Open"}
+    schemes_data = fetch_data(url, params)
 
-    query_params = {"Scheme_Type": "Open"}
-
-    response = requests.get(url, headers=headers, params=query_params)
-
-    if response.status_code == 200:
-        schemes_data = response.json()
-        schemes = [
-            MutualFundScheme(**scheme).dict() for scheme in schemes_data
-        ]
-        families = list(set([scheme.get('mutual_fund_family') for scheme in schemes]))
-        families.sort()
-        return {"families": families}
-    else:
-        raise HTTPException(
-            status_code=response.status_code,
-            detail="Failed to fetch all schemes"
-        )
+    schemes = [MutualFundScheme(**scheme).dict() for scheme in schemes_data]
+    families = sorted(set(scheme.get('mutual_fund_family') for scheme in schemes))
+    return {"families": families}
 
 
 @app.get("/fund-schemes", dependencies=[Depends(authenticate_access_token)])
@@ -69,25 +72,12 @@ def get_fund_schemes(family: str):
     """
     Endpoint to get details for the perticular fund family
     """
-    url = base_url + "/latest"
+    url = f"{base_url}/latest"
+    params = {"Scheme_Type": "Open", "Mutual_Fund_Family": family}
+    schemes_data = fetch_data(url, params)
 
-    query_params = {
-        "Scheme_Type": "Open",
-        "Mutual_Fund_Family": family
-    }
-
-    response = requests.get(url, headers=headers, params=query_params)
-    if response.status_code == 200:
-        schemes_data = response.json()
-        schemes = [
-            MutualFundScheme(**scheme).dict() for scheme in schemes_data
-        ]
-        return {"schemes": schemes}
-    else:
-        raise HTTPException(
-            status_code=response.status_code,
-            detail="Failed to fetch fund family"
-        )
+    schemes = [MutualFundScheme(**scheme).dict() for scheme in schemes_data]
+    return {"schemes": schemes}
 
 
 @app.post("/purchase", dependencies=[Depends(authenticate_access_token)])
@@ -95,30 +85,15 @@ def purchase_units(isin: str, units: int):
     """
     Endpoint to purchase of units for a selected mutual fund
     """
-    url = base_url + "/master"
+    url = f"{base_url}/master"
+    params = {"ISIN": isin}
+    respons_json = fetch_data(url, params)
 
-    querystring = {"ISIN": isin}
+    if not respons_json:
+        return {"status": 503, "message": "No records found!"}
 
-    response = requests.get(url, headers=headers, params=querystring)
-    if response.status_code == 200:
-        respons_json = response.json()
-        if not respons_json:
-            return {
-                "status": 503,
-                "message": "No records found !"
-            }
-        for option in respons_json:
-            if option["Purchase_Allowed"] and option["Minimum_Purchase_Amount"] >= units:
-                return {
-                    "status": 200,
-                    "message": f"Successfully purchased {units} units"
-                }
-        return {
-            "status": 400,
-            "message": "Units not satisfying the minimum desired values"
-        }
-    else:
-        raise HTTPException(
-            status_code=response.status_code,
-            detail="Unexpected error occured"
-        )
+    for option in respons_json:
+        if option["Purchase_Allowed"] and option["Minimum_Purchase_Amount"] <= units:
+            return {"status": 200, "message": f"Successfully purchased {units} units"}
+
+    return {"status": 400, "message": "Units do not meet the minimum purchase requirements"}
